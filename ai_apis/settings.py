04df1598cbd0479d47ff002f7636319bb8c91d37
rkatre1234 <152957114+rkatre1234@users.noreply.github.com>
2025-03-06 14:55:30 +0530

    allow ip

--- ai_apis/settings.py
+++ ai_apis/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-jid!i*)))!2q$%-6qxnf&aixj8)=hpkzck7_a4y%7dm2vgx5!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.86.191.187', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['3.93.231.224', 'localhost', '127.0.0.1']
 
 
 # Application definition
